3b94681746f883a847d4a7d61b31410f7e64f755
Dimo Mitev <dimosoftuni@gmail.com>
2024-10-03 20:53:13 +0300

    fix tests

--- tests/home.tests.js
+++ tests/home.tests.js
@@ -5,7 +5,7 @@ suite('Home page', function() {
   test('Page title', async function() {
     let res = await fetch("http://localhost:8888/");
     let body = await res.text();
-    assert.ok(body.includes("<h1>DevOps Registry App</h1>"));
+    assert.ok(body.includes("<h1>The New DevOps Registry App</h1>"));
   });
   
   test('Students count', async function() {
