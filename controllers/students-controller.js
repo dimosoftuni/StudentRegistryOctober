bb838ac7c1a4a4117be9316c888dbffc525074ca
Dimo Mitev <dimosoftuni@gmail.com>
2024-10-03 20:33:07 +0300

    update title

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -2,7 +2,7 @@ function setup(app, students) {
   app.get('/', function(req, res) {
     let model = {
       title: "MVC Example",
-      msg: "Students Registry",
+      msg: "DevOps Registry",
       students: students
     };
     res.render('home', model);
